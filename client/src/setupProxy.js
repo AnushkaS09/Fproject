d25593a655d41537bc4866f8167c0db50d924387
AnushkaS09 <anushkashrivastava95098@gmail.com>
2023-04-18 16:23:33 +0530

    second commit

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app){
+    app.use(createProxyMiddleware('/auth/google',{target:"http://127.0.0.1:5000/"}));
+    app.use(createProxyMiddleware('/api',{target:"http://127.0.0.1:5000/"}));
+    app.use(createProxyMiddleware('/api/user/*',{target:"http://127.0.0.1:5000/"}));
+};
